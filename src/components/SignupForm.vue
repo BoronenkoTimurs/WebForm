ae617563f0a65bfd50660287de63608aae71db2f
timurcsdd <timurubica@bk.ru>
2023-08-13 00:41:35 +0300

    Bild role,skills,checkbox and delete btn

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -19,9 +19,44 @@
       required
       v-model="password"
     />
+
+    <label class="text-slate-400 uppercase font-bold">Role: </label>
+    <select
+      class="block py-3 mb-2 outline-none px-2 w-full box-border border-solid"
+      v-model="role"
+    >
+      <option value="developer">Web Developer</option>
+      <option value="designer">Web Designer</option>
+    </select>
+
+    <label class="text-slate-400 uppercase font-bold">Skill:</label>
+    <input
+      class="block py-3 mb-2 outline-none px-2 w-full box-border border-solid border-b-2 border-slate-800 text-slate-600"
+      type="text"
+      v-model="tempSkill"
+      @keyup="addSkill"
+    />
+    <div v-for="skill in skills" :key="skill">
+      {{ skill }}
+    </div>
+    <!-- TODO: change "relative and left-40" for normal center position -->
+    <button @click="removeSkill" class="bg-slate-300 rounded-md p-2 relative left-40">Delete</button>
+    <div>
+      <input
+        v-model="termns"
+        class="inline-block w-4 mr-2.5 relative top-0.5"
+        type="checkbox"
+        required
+      />
+      <label class="text-slate-400 uppercase font-mono"
+        >Accept terms and conditions!</label
+      >
+    </div>
   </form>
   <p class="text-center">Email: {{ email }}</p>
   <p class="text-center">Password: {{ password }}</p>
+  <p class="text-center">Role: {{ role }}</p>
+  <p class="text-center">Termns: {{ termns }}</p>
 </template>
 <script>
 export default {
@@ -29,7 +64,25 @@ export default {
     return {
       email: "",
       password: "",
+      role: "designer",
+      termns: "",
+      tempSkill: "",
+      skills: [],
     };
+  },
+  methods: {
+    addSkill(e) {
+      if (e.key === "," && this.tempSkill) {
+        if (!this.skills.includes(this.tempSkill)) {
+          this.skills.push(this.tempSkill);
+          this.tempSkill = "";
+        }
+        this.tempSkill = "";
+      }
+    },
+    removeSkill(e){
+        this.skills.pop()
+    }
   },
 };
 </script>
